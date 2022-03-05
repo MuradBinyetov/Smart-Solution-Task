c6e314186318701e46987cf82561b1d7c46cbed2
MuradBinyetov <murad_binyetov@mail.ru>
2022-03-05 19:13:41 +0400

    MVC Project created, Identity service added

---
+++ SmartSolutionTask/SmartSolutionTask/Models/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartSolutionTask.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+        [StringLength(255)] 
+        public string Name { get; set; }
+
+        [StringLength(255)]
+        public string Surname { get; set; }
+    }
+}
---
+++ SmartSolutionTask/SmartSolutionTask/Models/SystemRoles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartSolutionTask.Models
+{
+    public sealed class SystemRoles
+    {
+        public const string Admin = "Admin";
+        public const string OrganizationRole = "Organization";
+        public const string WorkerRole = "Organization";
+    }
+}
